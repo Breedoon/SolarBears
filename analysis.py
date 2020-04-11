25b636ace88b7bbd872d709a0a10e3e8f3c4839b
Breedoon <breedoon@programmer.net>
2020-04-11 15:29:16 -0700

    Optimization of downloading data from database

--- analysis.py
+++ analysis.py
@@ -13,7 +13,7 @@ import os
 from fetchers.fetcher_csv import get_site_production, collect_data
 from db.helper_db import run_query, run_queries
 from misc.mapper import plot_map
-from misc.helper import time_batches
+from misc.helper import time_batches, normalize
 
 CACHE_DIR = 'efficiency_cache'
 
@@ -37,91 +37,6 @@ def choose_subset(radius):
     return list(map(int, solectria_sites['site_id'].values))
 
 
-# production - dataframe with columns 'value' and 'date' for production in Wh and date in format '2020-01-20 10:00:00'
-# if interval == None, assumes production is sorted by 'date', and calculates interval based on first two 'date's
-def df_to_efficiency(production, size, interval=None):
-    # time interval in seconds for conversion to watts; based on production[1] - production[0]
-    if not interval:
-        interval = sum((np.array(list(map(int, str(production['date'][1]).split(' ')[1].split(':'))))
-                        - np.array(list(map(int, str(production['date'][0]).split(' ')[1].split(':')))))
-                       * np.array([3600, 60, 1]))
-    # efficiency = W / W_max; W = Wh / h; W_max = system size * 1000 (kW to W)
-    production['efficiency'] = production['value'].div(interval / 3600 * size * 1000)
-    production['efficiency'].fillna(0, inplace=True)
-    return production
-
-
-# production - dataframe with columns 'value' and 'date' for production in Wh and date in format '2020-01-20 10:00:00'
-# interval in seconds
-def daily_efficiency(production, size, interval):
-    efficiency = df_to_efficiency(production, size, interval)
-    result = {}
-    n_points = {}
-    for eff, date in zip(efficiency['efficiency'], efficiency['date']):
-        try:
-            result[datetime(date.year, date.month, date.day)] += eff
-            n_points[datetime(date.year, date.month, date.day)] += 1
-        except KeyError:
-            result[datetime(date.year, date.month, date.day)] = eff
-            n_points[datetime(date.year, date.month, date.day)] = 1
-    for dt in result:
-        result[dt] = result[dt] / n_points[dt]
-    return pd.DataFrame(data=list(result.values()), index=result.keys(), columns=['value'])
-
-
-# returns a file name to store daily efficiencies for a given site for given time period
-def get_filename(site_id, start, end):
-    return CACHE_DIR + '/' + "_".join([str(site_id), str(start), str(end)]).replace(':', '=') + '.csv'
-
-
-# conducts a two-tailed t-test comparing two
-# 'sites_1' and 'sites_2' - lists of site_ids; 'start', 'end' - datetime objects
-# interval in minutes
-def average_daily_efficiency(site_ids, start, end, interval):
-    site_metadatas = run_query(
-        'SELECT site_id, size FROM site WHERE site_id IN ' + '(' + ("'{}', " * len(site_ids))[:-2].format(
-            *site_ids) + ')', True).set_index('site_id')
-    # site_metadatas = run_queries(["SELECT * FROM site WHERE site_id = '{}'".format(site_id) for site_id in site_ids])
-    system_sizes = [float(site_metadatas['size'][str(id)]) for id in site_ids]
-
-    if not os.path.exists(CACHE_DIR):
-        os.makedirs(CACHE_DIR)
-    existing_files = [CACHE_DIR + '/' + f for f in os.listdir(CACHE_DIR)]
-    expected_files = [get_filename(site_id, start, end) for site_id in site_ids]
-    old_files = []
-    efficiencies = [[]] * len(site_ids)
-    new_site_ids = []
-    old_site_ids = []
-    for i in range(len(site_ids)):
-        if expected_files[i] not in existing_files:
-            new_site_ids.append(i)
-        else:
-            old_site_ids.append(i)
-            old_files.append(expected_files[i])
-    new_datas = run_queries(["SELECT * FROM production WHERE site_id = '{site_id}' AND date "
-                         ">= '{start}'::timestamp and date < '{end}'::timestamp"
-                        .format(site_id=site_id, start=str(start), end=str(end))
-                         for site_id in [site_ids[i] for i in new_site_ids]], True)
-    new_efficiencies = [daily_efficiency(data, size, interval * 60) for data, size in zip(new_datas, system_sizes)]
-    old_efficiencies = [pd.read_csv(f).set_index('date') for f in old_files]
-    for i in range(len(new_site_ids)):
-        efficiencies[new_site_ids[i]] = new_efficiencies[i]
-        new_efficiencies[i].to_csv(get_filename(site_ids[new_site_ids[i]], start, end), index_label='date')
-    for i in range(len(old_site_ids)):
-        efficiencies[old_site_ids[i]] = old_efficiencies[i]
-
-    total_effs = [np.mean(eff['value']) for eff in efficiencies]  # mean efficiency for each site over the whole period
-
-    # for i in range(len(datas)):  # checking if any of the sites doesn't cover the entire period from start to end
-    #     if min(datas[i]['date']) != start or max(datas[i]['date']) != end - relativedelta(minutes=interval) or \
-    #             len(datas[i]) < (end - start) / timedelta(minutes=interval):
-    #         print(site_ids[i], min(datas[i]['date']), max(datas[i]['date']), len(datas[i]))
-
-    # total_effs = np.concatenate(tuple(eff['value'].values for eff in efficiencies))  # day as a sample unit
-
-    return total_effs
-
-
 def plot_sites(solaredge_sites=None, solectria_sites=None):
     if solaredge_sites is None:
         solaredge_sites = pd.read_csv('csv/solaredge_sites.csv')
@@ -132,6 +47,16 @@ def plot_sites(solaredge_sites=None, solectria_sites=None):
     solectria_sites.insert(0, 'source', 'solectria')
     df = pd.concat([solaredge_sites, solectria_sites], ignore_index=True)
     plot_map(df)
+
+
+def plot_annual_performance(solaredge_effs, solectria_effs):
+    plt.plot_date(pd.to_datetime(solaredge_effs.columns), normalize(np.mean(solaredge_effs), 30),
+                  linestyle='solid', marker=None, label='Solaredge', color='orange')
+    plt.plot_date(pd.to_datetime(solectria_effs.columns), normalize(np.mean(solectria_effs), 30),
+                  linestyle='solid', marker=None, label='Solectria', color='purple')
+    plt.xlabel('Date')
+    plt.ylabel('Performance')
+    plt.show()
 
 
 def solectria_to_database(solectria_ids):
@@ -151,14 +76,139 @@ def solectria_to_database(solectria_ids):
     print(fetching_times)
 
 
-if __name__ == '__main__':
-    solaredge_ids = run_query("SELECT site_id FROM site WHERE length(site_id) > 4 AND state = 'MA'", True).values
-    solectria_ids = choose_subset(0.2125)
-    new_sites = set(solectria_ids) - set(map(int, run_query('SELECT site_id FROM site', True).values))
-    solectria_to_database(new_sites)
+def copy_from_main_db():
+    from fetchers.fetcher_csv import store
+    from math import ceil
+    site = run_query('SELECT * FROM site', retrieve=True)
+    store('site', site, bulk_load=True)
+    component_details = run_query('SELECT * FROM component_details', retrieve=True)
+    store('component_details', component_details, bulk_load=True)
+    n = run_query('SELECT count(*) FROM production')[0]['count']
+    l = 500000
+    for i in range(ceil(n / l)):
+        production = run_query('SELECT * FROM production LIMIT {} OFFSET {}'.format(l, i * l), retrieve=True)
+        production.fillna(0, inplace=True)
+        production["value"] = production["value"].astype(int)
+        store('production', production, bulk_load=True)
 
-    solaredge_effs = average_daily_efficiency(solaredge_ids, datetime(2019, 1, 1), datetime(2020, 1, 1), 15)
-    print("Solaredge efficiencies:", solaredge_effs)
-    solectria_effs = average_daily_efficiency(solectria_ids, datetime(2019, 1, 1), datetime(2020, 1, 1), 60)
-    print("Solectria efficiencies:", solectria_effs)
-    print("P-value:", ttest_ind(solaredge_effs, solectria_effs)[1])
+
+# converts production to power
+# production - dataframe with columns 'value' and 'date' for production in Wh and date in format '2020-01-20 10:00:00'
+# if interval == None, assumes production is sorted by 'date', and calculates interval based on first two 'date's
+def df_to_power(production, interval=None):
+    # time interval in seconds for conversion to watts; based on production[1] - production[0]
+    if not interval:
+        interval = sum((np.array(list(map(int, str(production['date'][1]).split(' ')[1].split(':'))))
+                        - np.array(list(map(int, str(production['date'][0]).split(' ')[1].split(':')))))
+                       * np.array([3600, 60, 1]))
+    # efficiency = W / W_max; W = Wh / h; W_max = system size * 1000 (kW to W)
+    production['value'] = production['value'].div(interval / 3600)
+    # production['value'].fillna(0, inplace=True)
+    return production
+
+
+def df_to_efficiency(production, size, interval=None):
+    return df_to_power(production).div(size * 1000)
+
+
+# production - dataframe with columns 'value' and 'date' for production in Wh and date in format '2020-01-20 10:00:00'
+# interval in seconds
+def daily_efficiency(production, size, interval):
+    efficiency = df_to_efficiency(production, size, interval)
+    result = {}
+    n_points = {}
+    for eff, date in zip(efficiency['efficiency'], efficiency['date']):
+        try:
+            result[datetime(date.year, date.month, date.day)] += eff
+            n_points[datetime(date.year, date.month, date.day)] += 1
+        except KeyError:
+            result[datetime(date.year, date.month, date.day)] = eff
+            n_points[datetime(date.year, date.month, date.day)] = 1
+    for day in result:
+        result[day] = result[day] / n_points[day]
+    return pd.DataFrame(data=list(result.values()), index=result.keys(), columns=['value'])
+
+
+# returns a file name to store daily efficiencies for a given site for given time period
+def get_filename(site_id, start, end):
+    return CACHE_DIR + '/' + "_".join([str(site_id), str(start), str(end)]).replace(':', '=') + '.csv'
+
+
+# conducts a two-tailed t-test comparing two
+# 'sites_1' and 'sites_2' - lists of site_ids; 'start', 'end' - datetime objects
+# interval in minutes
+def average_daily_efficiency(site_ids, start, end, interval):
+    site_ids = list(map(str, site_ids))
+
+    site_metadatas = run_query(
+        'SELECT site_id, size FROM site '
+        'WHERE site_id IN ' + '(' + ("'{}', " * len(site_ids))[:-2].format(*site_ids) + ')', True).set_index('site_id')
+    system_sizes = [float(site_metadatas['size'][str(id)]) for id in site_ids]
+
+    if not os.path.exists(CACHE_DIR):
+        os.makedirs(CACHE_DIR)
+    existing_files = [CACHE_DIR + '/' + f for f in os.listdir(CACHE_DIR)]
+    expected_files = [get_filename(site_id, start, end) for site_id in site_ids]
+
+    efficiencies = [[]] * len(site_ids)
+    new_sites = {}
+
+    for i in range(len(site_ids)):
+        if expected_files[i] not in existing_files:
+            new_sites[site_ids[i]] = i
+        else:
+            efficiencies[i] = pd.read_csv(expected_files[i]).set_index('date')
+    if len(new_sites) > 0:
+        new_datas = run_query(
+            "SELECT site_id, date, value FROM pss.public.production "
+            "WHERE date >= '{start}'::timestamp and date < '{end}'::timestamp AND "
+            .format(start=str(start), end=str(end)) +
+            "site_id IN " + "(" + ("'{}', " * len(new_sites))[:-2].format(*new_sites) + ')', True)
+
+        daily_power_per_site = {}
+        n_elements_per_site = {}
+        daily_efficiency_per_site = {}
+        powers = df_to_power(new_datas, interval * 60)
+        for site_id, date, value in zip(powers['site_id'], powers['date'], powers['value']):
+            if not daily_power_per_site.get(site_id):
+                daily_power_per_site[site_id] = {}
+                n_elements_per_site[site_id] = {}
+            day = date._date_repr
+            if not daily_power_per_site[site_id].get(day):
+                daily_power_per_site[site_id][day] = value
+                n_elements_per_site[site_id][day] = 1
+                continue
+            daily_power_per_site[site_id][day] += value
+            n_elements_per_site[site_id][day] += 1
+
+        for site_id in daily_power_per_site:
+            daily_efficiency_per_site[site_id] = {}
+            for day in daily_power_per_site[site_id]:
+                daily_efficiency_per_site[site_id][day] = daily_power_per_site[site_id][day] \
+                                                          / n_elements_per_site[site_id][day] \
+                                                          / (system_sizes[new_sites[site_id]] * 1000)
+            efficiencies[new_sites[site_id]] = pd.DataFrame(data=list(daily_efficiency_per_site[site_id].values()),
+                                                            index=daily_efficiency_per_site[site_id].keys(),
+                                                            columns=['value'])
+            efficiencies[new_sites[site_id]].to_csv(get_filename(site_id, start, end), index_label='date')
+
+    total_effs = efficiencies[0].rename(columns={"value": site_ids[0]}).transpose()
+    for i in range(1, len(efficiencies)):
+        total_effs = total_effs.append(efficiencies[i].rename(columns={"value": site_ids[i]}).transpose(), sort=True)
+    total_effs.fillna(0, inplace=True)
+
+    return total_effs
+
+
+if __name__ == '__main__':
+    solaredge_ids = list(zip(*run_query(
+        "SELECT site_id FROM site WHERE length(site_id) > 4 AND state = 'MA'", True).values))[0]
+    solectria_ids = list(set(choose_subset(0.2125)).intersection(set(map(int, list(zip(*run_query(
+        "SELECT site_id FROM site WHERE length(site_id) <= 4", True).values))[0]))))
+
+    start = datetime(2018, 1, 1)
+    end = datetime(2020, 1, 1)
+    solectria_effs = average_daily_efficiency(solectria_ids, start, end, 60)
+    solaredge_effs = average_daily_efficiency(solaredge_ids, start, end, 15)
+
+    plot_annual_performance(solectria_effs, solaredge_effs)
